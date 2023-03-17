81bb33c448e0d15b315ffa85dcd3af7c9e2e01bb
Bryon F <morosmage@yahoo.com>
2023-03-17 11:01:44 -0500

    updated authservice, add environments

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user';
-import { envi }
+import { environment } from 'src/environments/environment.development';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  authUrl: string = `${enviroment.baseUrl}/auth`;
+  authUrl: string = `${environment.baseUrl}/auth`;
   loggedIn: boolean = false;
-  currentUser?: User | null;
+  currUser?: User | null;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
+  login(email: string, password: string): Observable<any> {
+    const payload = { email, password };
+
+    return this.http.post<any>(`${this.authUrl}/login`, payload, {
+      headers: environment.headers,
+    }).pipe(tap(response => {
+      if (response) {
+        this.currUser = new User();
+      }
+    }));
+  }
+
+  register(userName: string, email: string, pass: string): Observable<any> {
+    const payload = { userName, email, pass };
+
+    return this.http.post<any>(`${this.authUrl}`, payload, { headers: environment.headers }).pipe(
+      tap(response => {
+        if (response) {
+          this.currUser = new User();
+        }
+      })
+    )
+  }
+
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    baseUrl: "https://localhost:7031",
+    headers: {
+        'Content-Type': 'application/json',
+    },
+};
